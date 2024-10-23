2c1a1bcf3bc01e87aef44ca0a5d44a6fc7115125
jacobbolano <jacobcbolano@gmail.com>
2024-10-22 17:31:11 -0700

    licm implementation works

--- examples/task3/licm.py
+++ examples/task3/licm.py
@@ -87,34 +87,6 @@ def fix_loops(index_changed, loops, loop_key_changed):
                 preheader += 1
         latch = loop["latch"]
         latch = latch if latch < index_changed else latch + 1
-        # start = header if preheader == "TRIVIAL" else preheader
-
-
-        # logging.debug(f"looking at header {header} and start {start} for loop {loop} where index changed is {index_changed}")
-        # if start <= index_changed and index_changed <= latch:
-        #     logging.debug(f" had to update loop due to index in betweeen")
-
-        #     # we check if its at header or preheader - this shouldnt happen
-        #     if index_changed == header:
-        #         logging.debug(f"this shouldnt happen. Our index changed was {index_changed}, and the loop was previously {loop_components}. we then are changing our header {header}")
-        #         header += 1
-        #     if index_changed == preheader:
-        #         logging.debug(f"this shouldnt happen. Our index changed was {index_changed}, and the loop was previously {loop_components}. we then are changing our preheader {preheader}")
-        #         preheader += 1
-
-        #     # our index changed is in betweeen - its part of this loop
-        #     # this means our latch increased by 1 and our total components did as well
-        #     latch += 1
-        #     loop_components.append(latch)
-            
-        # elif start > index_changed:
-        #     logging.debug(f" had to update loop due to index before")
-        #     # index changed is before our loop - we must shift everything in the loop
-        #     loop_components = [loop_component + 1 for loop_component in loop_components] # first update loop components
-        #     header += 1
-        #     if preheader != "TRIVIAL":
-        #         preheader += 1
-        #     latch += 1
         
         # update loops (Add to new loops)
         new_loops[loop_key] = {
@@ -131,13 +103,17 @@ def normalize_loops(loops, blocks, label_to_block, cfg):
     loop_keys = list(loops.keys())
     for loop_key in loop_keys:
         curr_loop = loops[loop_key]
-        # logging.debug(f"loop before adding potential preheader {curr_loop}")
         cur_loop_header = curr_loop["header"]
 
         # pre header potentials are a predecessor but they are also earlier than the header block
-        logging.debug(f"cfg prior to checking preds {cfg}")
-        preds_header = [preds for preds in cfg[cur_loop_header]["predecessors"] if preds < cur_loop_header]
-        logging.debug(f"these are potential preheaders {preds_header}")
+        all_loops = set()
+        for lp_key in loop_keys:
+            if loops[lp_key]["header"] == cur_loop_header:
+                all_loops.update(loops[lp_key]["loop_components"])
+
+        preds_header = [preds for preds in cfg[cur_loop_header]["predecessors"] if preds not in all_loops]
+
+
         if len(preds_header) != 1:
             # there is more than one predecessor to a header
             # we need to insert a preheader
@@ -158,64 +134,46 @@ def normalize_loops(loops, blocks, label_to_block, cfg):
             label_to_block[pre_header[0]['label']] = index_pre_header # update map that goes from labels to block index
 
             # update current loop components because they are now shifted
-            curr_loop["loop_components"] = [cp + 1 for cp in curr_loop["loop_components"]]
+            curr_loop["loop_components"] = [cp + 1 if cp >= index_pre_header else cp for cp in curr_loop["loop_components"]]
             curr_loop["header"] += 1
-            curr_loop["latch"] += 1
+            curr_loop["latch"] = curr_loop["latch"] + 1 if curr_loop["latch"] >= index_pre_header else curr_loop["latch"]
 
             # we need to fix the control flow. predecessor blocks earlier than our header now point to preheader.
+            preds_header = [pd if pd < index_pre_header else pd + 1 for pd in preds_header]
             for pred in preds_header:
-                if pred < index_pre_header: # it occured before our header, so we need to make sure it points to our preheader
-                    pred_block = blocks[pred]
-                    last_instruction = pred_block[-1]
-                    if "op" in last_instruction:
-                        if last_instruction["op"] == "jmp":
-                            last_instruction["labels"] = [pre_header[0]["label"]]
-                        elif last_instruction["op"] == "br":
-                            labels = []
-                            for label in last_instruction["labels"]:
-                                if label == curr_loop_header_label: # the label in the branch was previously our header
-                                    labels.append(pre_header[0]["label"])
-                                else:
-                                    labels.append(label)
-                            last_instruction["labels"] = labels
-                    blocks[pred][-1] = last_instruction
+                pred_block = blocks[pred]
+                if "preheader" in pred_block[0]["label"]:
+                    continue # this is a preheader so dont change the logic
+                last_instruction = pred_block[-1]
+                if "op" in last_instruction:
+                    if last_instruction["op"] == "jmp":
+                        last_instruction["labels"] = [pre_header[0]["label"]]
+                    elif last_instruction["op"] == "br":
+                        labels = []
+                        for label in last_instruction["labels"]:
+                            if label == curr_loop_header_label: # the label in the branch was previously our header
+                                labels.append(pre_header[0]["label"])
+                            else:
+                                labels.append(label)
+                        last_instruction["labels"] = labels
+                blocks[pred][-1] = last_instruction
                 # if there is no op or its not a jump style instruciton, then it will "fall" to the preheader naturally
             cfg = create_cfg(blocks, label_to_block) # update our cfg based on new blocks and label to block 
-            logging.debug(f"updated cfg {cfg}")
 
-            # additionally we need to fix phi labels that previously point to the header now go through the preheader
-            for cp in curr_loop["loop_components"]:
-                cp_block = blocks[cp]
-                for instr in cp_block:
-                    if "op" in instr and instr["op"] == "phi":
-                        # we check the labels of this phi function and see if they now go through the preheader
-                        new_labels = []
-                        for label in instr["labels"]:
-                            phi_label_block = label_to_block[label]
-                            if phi_label_block in preds_header: # this block was a previous predecessor
-                                # this meanas it now flows through our phi function
-                                new_labels.append(pre_header[0]["label"])
-                            else: # this was not a predecessor, label it normally
-                                new_labels.append(label)
-                        instr["labels"] = new_labels
+            # update phis if they now are different predecessor
+            for instr in blocks[curr_loop["header"]]:
+                if "op" in instr and instr["op"] == "phi": # check if its a phi
+                    # we check the labels of this phi function and see if they now go through the preheader
+                    new_labels = []
+                    for label in instr["labels"]:
+                        phi_label_block = label_to_block[label]
+                        if phi_label_block in preds_header and "preheader" not in label: # this block label was a previous predecessor to header
+                            # this meanas it now flows through our preheader
+                            new_labels.append(pre_header[0]["label"])
+                        else: # this was not a predecessor, label it normally
+                            new_labels.append(label)
+                    instr["labels"] = new_labels
 
-            # EDIT: move phis to preheader
-            # to_remove = []
-            # for instr in blocks[curr_loop["header"]]:
-            #     if "op" in instr and instr["op"] == "phi":
-            #         to_remove.append(instr)
-            
-            # for instr in to_remove:
-            #     last_instruction = blocks[curr_loop["header"]][-1]
-            #     if "op" in last_instruction and last_instruction["op"] == "jmp" or last_instruction["op"] == "br":
-            #         # then we need to insert before this jmp
-            #         blocks[index_pre_header].insert(len(blocks[index_pre_header]) - 1, instr)
-            #     else:
-            #         blocks[index_pre_header].append(instr)
-                
-            #     blocks[curr_loop["header"]].remove(instr)
-
-            logging.debug(f"Inserted a pre header {pre_header} with index {index_pre_header} for loop {curr_loop}")
             # update other loops
             loops = fix_loops(index_pre_header, loops, loop_key)
         else:
@@ -224,7 +182,6 @@ def normalize_loops(loops, blocks, label_to_block, cfg):
         # add pre header
         curr_loop["preheader"] = index_pre_header
         loops[loop_key] = curr_loop
-        # logging.debug(f"updated preheader {index_pre_header} for loop {curr_loop}")
     return loops, blocks, label_to_block
 
 def perform_licm(normalized_loops, blocks):
@@ -289,32 +246,16 @@ def perform_licm(normalized_loops, blocks):
 
         loop = normalized_loops[loop_key]
         preheader = loop["preheader"]
-        logging.debug(f" we are hoisting instruction {instruction} to preheader {preheader}")
         # first add it to preheader
         # check if last instruction is a jmp style instruction
         last_instruction = blocks[preheader][-1]
-        if "op" in last_instruction and last_instruction["op"] == "jmp" or last_instruction["op"] == "br":
+        if "op" in last_instruction and (last_instruction["op"] == "jmp" or last_instruction["op"] == "br"):
             # then we need to insert before this jmp
             blocks[preheader].insert(len(blocks[preheader]) - 1, instruction)
         else:
             blocks[preheader].append(instruction)
         # next remove from original block in loop
-        # logging.debug(f"trying to remove {instruction} from {blocks[block]}")
         blocks[block].remove(instruction)
-
-        # we need to fix phis at this moment
-        # for b in blocks:
-        #     for phi_instr in b:
-        #         if "op" in phi_instr and phi_instr["op"] == "phi":
-        #             for arg, label in zip(phi_instr["args"], phi_instr["labels"]):
-        #                 if "dest" in instruction and instruction["dest"] == arg:
-        #                     # logging.debug("fixing phis")
-        #                     # then the argument we are trying to hoist has a phi, so we update the corresponding label
-        #                     phi_instr["labels"].remove(label) # remove old label
-        #                     phi_instr["args"].remove(arg) # remove arg (just to keep order)
-
-        #                     phi_instr["labels"].append(blocks[preheader][0]["label"]) # add new label
-        #                     phi_instr["args"].append(arg) # add back argument
 
     return blocks
 
@@ -325,19 +266,12 @@ def find_licm(instructions):
     # blocks, label_to_block = add_pseudo_labels(blocks, label_to_block) # add any pseudo labels for unlabeled code
     # blocks, label_to_block = add_entry_blocks(blocks, label_to_block) # add an entry block if not already there
     cfg = create_cfg(blocks, label_to_block) # create our CFG
-    # for i, block in enumerate(blocks):
-    #     logging.debug(f"block {i}: {block}")
-    # logging.debug(f"initial cfg {cfg}")
     dominators = find_dominators(blocks, cfg) # maps block_index: set of dominating blocks_indices
     back_edges = find_back_edges(dominators, blocks, cfg) # find back edges
-    #logging.debug(f"back edges {back_edges}")
+
     loops = find_loops(back_edges, cfg) # find loops    
-    logging.debug(f"initial loops found {loops}")
+
     normalized_loops, blocks, label_to_block = normalize_loops(loops, blocks, label_to_block, cfg) # normalize loops
-    # logging.debug(f"blocks updated after normalizing {blocks}")
-    # logging.debug(f'this is label to block {label_to_block}')
-    logging.debug(f"normalized loops {normalized_loops}")
-    #logging.debug(f"cfg updated after loops normalized: {cfg}")
     # NOTE: if we want just normalized loops we return now
     # return rebuild_instructions(blocks)
     # NOTE: otherwise we find licm and move them   
